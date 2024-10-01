064579006358df2879d89044d4a129cdc4727e21
Marcos Lilljedahl <marcosnils@gmail.com>
2024-10-01 17:10:02 -0300

    wip: add Dagger CI support
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

---
+++ modules/gha/main.go
@@ -0,0 +1,276 @@
+// A generated module for Gha functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"dagger/gha/internal/dagger"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/google/go-github/v61/github"
+)
+
+type Gha struct {
+	PullRequest *PullRequest
+}
+
+type Payload struct {
+}
+
+type Event struct {
+	Payload   dagger.JSON
+	EventType string
+}
+
+type PullRequest struct {
+
+	// Action is the action that was performed. Possible values are:
+	// "assigned", "unassigned", "review_requested", "review_request_removed", "labeled", "unlabeled",
+	// "opened", "edited", "closed", "ready_for_review", "locked", "unlocked", or "reopened".
+	// If the action is "closed" and the "merged" key is "false", the pull request was closed with unmerged commits.
+	// If the action is "closed" and the "merged" key is "true", the pull request was merged.
+	// While webhooks are also triggered when a pull request is synchronized, Events API timelines
+	// don't include pull request events with the "synchronize" action.
+	Action      string
+	Number      int
+	PullRequest PullRequestSpec
+
+	Repo  Repository
+	Label string
+}
+
+type PullRequestSpec struct {
+	Number         int
+	State          string
+	Locked         bool
+	CreatedAt      string
+	UpdatedAt      string
+	ClosedAt       string
+	MergedAt       string
+	Labels         []string
+	Merged         bool
+	Mergeable      bool
+	MergeableState string
+	Head           *PullRequestBranch
+	Base           *PullRequestBranch
+}
+
+type PullRequestBranch struct {
+	Label string
+	Ref   string
+	SHA   string
+	Repo  Repository
+}
+
+type PullRequestLabel struct {
+	Name string
+}
+
+type Repository struct {
+	Owner    User
+	Name     string
+	FullName string
+}
+
+type User struct {
+	Login    string
+	Name     string
+	UserType string
+}
+
+func New(ctx context.Context, eventSrc *dagger.File) (*Gha, error) {
+	contents, err := eventSrc.Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	ev := &Event{}
+	err = json.Unmarshal([]byte(contents), ev)
+	if err != nil {
+		return nil, err
+	}
+	ge, err := github.ParseWebHook(ev.EventType, []byte(ev.Payload))
+	if err != nil {
+		return nil, err
+	}
+
+	switch event := ge.(type) {
+	case *github.PullRequestEvent:
+		return &Gha{PullRequest: fromGithubPullRequest(event)}, nil
+	default:
+		return nil, errors.New("unsupported event type")
+
+	}
+
+}
+
+type Pipeline struct {
+	RunsOn  string
+	Changes []string
+	Name    string
+	Event   *PullRequest
+	Actions []Action
+}
+
+type PipelineRequest struct {
+	RunsOn string
+	Name   string
+	Event  *PullRequest
+}
+
+type Action string
+
+const (
+	PROpened      Action = "opened"
+	PRReopened    Action = "reopened"
+	PRSynchronize Action = "synchronize"
+)
+
+// Returns a container that echoes whatever string argument is provided
+func (m *Gha) WithPipeline(name string) *Pipeline {
+	return &Pipeline{Name: name, Event: m.PullRequest}
+}
+
+func (m *Pipeline) WithRunsOn(name string) *Pipeline {
+	m.RunsOn = name
+	return m
+}
+
+func (m *Pipeline) WithOnPullRequest(actions ...Action) *Pipeline {
+	m.Actions = actions
+	return m
+}
+
+func (m *Pipeline) WithOnChanges(paths ...string) *Pipeline {
+	m.Changes = paths
+	return m
+}
+
+func (m *Pipeline) Call(ctx context.Context, exec string) {
+
+	if m.Event == nil {
+		return
+
+	}
+
+	fmt.Println(exec)
+}
+
+func fromGithubPullRequest(e *github.PullRequestEvent) *PullRequest {
+	pr := &PullRequest{}
+	pr.Action = *e.Action
+	pr.Number = *e.Number
+
+	var createdAt, updatedAt, closedAt, mergedAt string
+	if e.PullRequest.CreatedAt != nil {
+		createdAt = e.PullRequest.CreatedAt.String()
+	}
+	if e.PullRequest.UpdatedAt != nil {
+		updatedAt = e.PullRequest.UpdatedAt.String()
+	}
+	if e.PullRequest.ClosedAt != nil {
+		closedAt = e.PullRequest.ClosedAt.String()
+	}
+	if e.PullRequest.MergedAt != nil {
+		mergedAt = e.PullRequest.MergedAt.String()
+	}
+
+	pr.PullRequest = PullRequestSpec{
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+		ClosedAt:  closedAt,
+		MergedAt:  mergedAt,
+		Labels:    []string{},
+	}
+
+	if e.PullRequest.Number != nil {
+		pr.PullRequest.Number = *e.PullRequest.Number
+	}
+	if e.PullRequest.State != nil {
+		pr.PullRequest.State = *e.PullRequest.State
+	}
+	if e.PullRequest.Locked != nil {
+		pr.PullRequest.Locked = *e.PullRequest.Locked
+	}
+	if e.PullRequest.Merged != nil {
+		pr.PullRequest.Merged = *e.PullRequest.Merged
+	}
+	if e.PullRequest.Mergeable != nil {
+		pr.PullRequest.Mergeable = *e.PullRequest.Mergeable
+	}
+	if e.PullRequest.MergeableState != nil {
+		pr.PullRequest.MergeableState = *e.PullRequest.MergeableState
+	}
+
+	if len(e.PullRequest.Labels) > 0 {
+		for _, label := range e.PullRequest.Labels {
+			pr.PullRequest.Labels = append(pr.PullRequest.Labels, *label.Name)
+		}
+	}
+
+	if e.PullRequest.Base != nil {
+		repo := Repository{
+			Owner: User{
+				Login:    *e.PullRequest.Base.Repo.Owner.Login,
+				UserType: *e.PullRequest.Base.Repo.Owner.Type,
+			},
+		}
+
+		if e.PullRequest.Base.Repo.Owner.Name != nil {
+			repo.Owner.Name = *e.PullRequest.Base.Repo.Owner.Name
+		}
+
+		pr.PullRequest.Base = &PullRequestBranch{
+			Label: *e.PullRequest.Base.Label,
+			Ref:   *e.PullRequest.Base.Ref,
+			SHA:   *e.PullRequest.Base.SHA,
+			Repo:  repo,
+		}
+	}
+
+	if e.PullRequest.Head != nil {
+		repo := Repository{
+			Owner: User{
+				Login:    *e.PullRequest.Head.Repo.Owner.Login,
+				UserType: *e.PullRequest.Head.Repo.Owner.Type,
+			},
+		}
+
+		if e.PullRequest.Head.Repo.Owner.Name != nil {
+			repo.Owner.Name = *e.PullRequest.Head.Repo.Owner.Name
+		}
+		pr.PullRequest.Head = &PullRequestBranch{
+			Label: *e.PullRequest.Head.Label,
+			Ref:   *e.PullRequest.Head.Ref,
+			SHA:   *e.PullRequest.Head.SHA,
+			Repo:  repo,
+		}
+	}
+
+	pr.Repo = Repository{
+		Owner: User{
+			Login: *e.Repo.Owner.Login,
+			Name:  *e.Repo.Name,
+		},
+	}
+
+	if e.Label != nil {
+		pr.Label = *e.Label.Name
+	}
+
+	return pr
+}
