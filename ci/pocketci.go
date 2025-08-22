1ce6b0833fecbeaed8cbad87ed9fd23390565d7a
Marcos Lilljedahl <marcosnils@gmail.com>
2025-08-22 12:42:46 -0300

    fix conflict
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- ci/pocketci.go
+++ ci/pocketci.go
@@ -7,7 +7,7 @@ import (
 
 func (m *Ci) Pipelines(ctx context.Context) *dagger.File {
 	// call hello for testing purposes
-	dag.Hello().Hello(ctx, "foo", "bar")
+	dag.Hello().Hello(ctx)
 	changes := []string{"**/**.go", "go.*"}
 	branches := []string{"main"}
 
